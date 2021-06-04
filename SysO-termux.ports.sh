2dfe3dadd95082724439f07c6469f0db5d9f9e23
Yisus7u7 <64093255+Yisus7u7@users.noreply.github.com>
2021-06-03 21:22:56 -0500

    Update SysO-termux.ports.sh

--- SysO-termux.ports.sh
+++ SysO-termux.ports.sh
@@ -2,6 +2,7 @@
 clear
 cd $HOME
 echo -e "\e[1;32m installing SysO-termux.ports \e[1m"
+sleep 4
 wget https://raw.githubusercontent.com/Yisus7u7/SysO-Termux/main/mirrors/dists/SysO-Termux/stable/binary-all/syso-termux-keyring_2.0.0_all.deb
 apt install ./syso-termux-keyring_2.0.0_all.deb
 rm $HOME/syso-termux-keyring_2.0.0_all.deb
